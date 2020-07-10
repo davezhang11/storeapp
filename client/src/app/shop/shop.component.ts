3cdebcca6c4abc017d36c9a42a8036fe19e1b882
Dave <ee.dave.zhang@gmail.com>
2020-07-09 22:22:38 -0400

    End of section 95

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,23 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from './shop.service';
 import { IProduct } from '../shared/models/product';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/productType';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
+
 export class ShopComponent implements OnInit {
   products: IProduct[];
-
-  constructor(private shopService: ShopService) { }
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
+  constructor(private shopService: ShopService) {}
 
   ngOnInit() {
-    this.shopService.getProducts().subscribe(response => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
   }
 
+  getProducts() {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe(
+      (response) => {
+        this.products = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  getBrands() {
+    this.shopService.getBrands().subscribe(
+      (response) => {
+        this.brands = [{id: 0, name: 'All'},  ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe(
+      (response) => {
+        this.types = [{id: 0, name: 'All'},  ...response];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
 }
